146dba57425e62734926b0d6e3cee15e3a464ab8
Kristoffer Brabrand <kristoffer@brabrand.no>
2023-02-13 09:14:34 +0100

    Add shallow option (#3)
    * Add shallow option
    
    * Update readme

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,40 @@ type SnakeCase<S extends string> = S extends `${infer T}${infer U}`
   ? `${T extends Capitalize<T> ? "_" : ""}${Lowercase<T>}${SnakeCase<U>}`
   : S;
 
-export type Snakify<T> = {
+export type Snakify<T, S = false> = {
   [K in keyof T as SnakeCase<string & K>]: T[K] extends Array<infer U>
     ? U extends ({} | undefined)
       ? Array<Snakify<U>>
       : T[K]
     : T[K] extends ({} | undefined)
-    ? Snakify<T[K]>
+    ? S extends true
+      ? T[K]
+      : Snakify<T[K]>
     : T[K];
 };
 
-function walk(obj): any {
+function walk(obj, shallow = false): any {
   if (!obj || typeof obj !== "object") return obj;
   if (obj instanceof Date || obj instanceof RegExp) return obj;
-  if (Array.isArray(obj)) return obj.map(walk);
+  if (Array.isArray(obj)) return obj.map(v => shallow ? v : walk(v));
 
   return Object.keys(obj).reduce((res, key) => {
     const camel = snakeCase(key);
-    res[camel] = walk(obj[key]);
+    res[camel] = shallow ? obj[key] : walk(obj[key]);
     return res;
   }, {});
 }
 
-export default function snakify<T>(
-  obj: T
-): T extends String ? string : Snakify<T> {
-  return typeof obj === "string" ? snakeCase(obj) : walk(obj);
+export default function snakify<T, S extends boolean = false>(
+  /**
+   * Value to be snakified
+   */
+  obj: T,
+
+  /**
+   * If true, only the top level keys of the obj will be camel cased
+   */
+  shallow?: S
+): T extends String ? string : Snakify<T, S> {
+  return typeof obj === "string" ? snakeCase(obj) : walk(obj, shallow);
 }
